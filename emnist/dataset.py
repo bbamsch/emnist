ccaa95e6bf1e688e82020f30fc25fcc046aae6d3
Brian Bamsch <bbamsch32@gmail.com>
2018-04-01 20:04:38 -0700

    Docstring and formatting changes for EMNIST dataset loader

--- emnist/dataset.py
+++ emnist/dataset.py
@@ -1,3 +1,7 @@
+"""
+Based on official tensorflow MNIST model loader found at
+https://github.com/tensorflow/models/blob/master/official/mnist/dataset.py
+"""
 import os
 import tensorflow as tf
 import numpy as np
@@ -5,37 +9,39 @@ import numpy as np
 
 def read32(bytestream):
     """Read 4 bytes from bytestream as an unsigned 32-bit integer."""
-    dt = np.dtype(np.uint32).newbyteorder('>')
-    return np.frombuffer(bytestream.read(4), dtype=dt)[0]
+    data_type = np.dtype(np.uint32).newbyteorder('>')
+    return np.frombuffer(bytestream.read(4), dtype=data_type)[0]
 
 
-def check_image_file_header(filename):
+def read_image_file_header(filename):
     """Validate that filename corresponds to images for the MNIST dataset."""
-    with tf.gfile.Open(filename, 'rb') as f:
-        magic = read32(f)
-        read32(f)  # num_images, unused
-        rows = read32(f)
-        cols = read32(f)
+    with tf.gfile.Open(filename, 'rb') as file:
+        magic = read32(file)
+        num_images = read32(file)
+        rows = read32(file)
+        cols = read32(file)
         if magic != 2051:
             raise ValueError('Invalid magic number %d in MNIST file %s' % (magic,
-                                                                           f.name))
+                                                                           file.name))
         if rows != 28 or cols != 28:
             raise ValueError(
                 'Invalid MNIST file %s: Expected 28x28 images, found %dx%d' %
-                (f.name, rows, cols))
+                (file.name, rows, cols))
+    return num_images, rows, cols
 
 
-def check_labels_file_header(filename):
+def read_labels_file_header(filename):
     """Validate that filename corresponds to labels for the MNIST dataset."""
-    with tf.gfile.Open(filename, 'rb') as f:
-        magic = read32(f)
-        read32(f)  # num_items, unused
+    with tf.gfile.Open(filename, 'rb') as file:
+        magic = read32(file)
+        num_labels = read32(file)
         if magic != 2049:
-            raise ValueError('Invalid magic number %d in MNIST file %s' % (magic,
-                                                                           f.name))
+            raise ValueError('Invalid magic number %d in MNIST file %s' % (magic, file.name))
+    return num_labels
 
 
 def dataset(directory, images_file, labels_file):
+    """tf.data.Dataset object for EMNIST images and labels file pair."""
     images_file = os.path.join(directory, images_file)
     labels_file = os.path.join(directory, labels_file)
 
@@ -44,20 +50,24 @@ def dataset(directory, images_file, labels_file):
     if not tf.gfile.Exists(labels_file):
         raise FileNotFoundError('Labels File was not found')
 
-    check_image_file_header(images_file)
-    check_labels_file_header(labels_file)
+    num_images, _, _ = read_image_file_header(images_file)
+    num_labels = read_labels_file_header(labels_file)
+    if num_images != num_labels:
+        raise RuntimeError('Mismatched number of images and labels')
 
     def decode_image(image):
-        # Normalize from [0, 255] to [0.0, 1.0]
+        """Normalize from [0, 255] to [0.0, 1.0]"""
         image = tf.decode_raw(image, tf.uint8)
         image = tf.cast(image, tf.float32)
         image = tf.reshape(image, [784])
         return image / 255.0
 
     def decode_label(label):
-        label = tf.decode_raw(label, tf.uint8)  # tf.string -> [tf.uint8]
-        label = tf.reshape(label, [])  # label is a scalar
-        return tf.to_int32(label)
+        """Transform label to one hot encoding"""
+        label = tf.decode_raw(label, tf.uint8)
+        label = tf.one_hot(label, 10)
+        label = tf.reshape(label, [10])
+        return label
 
     images = tf.data.FixedLengthRecordDataset(
         images_file, 28 * 28, header_bytes=16).map(decode_image)
